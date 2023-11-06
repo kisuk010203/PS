d40b7a151e8da0e798773351b9ad7213a319b3ef
kisuk010203 <chriskisuk@gmail.com>
2023-11-06 15:23:31 +0900

    Meet in the Middle

---
+++ C++/etc/Two_pointer/1044.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<long long, long long> pll;
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    int N;
+    cin >> N;
+    vector<ll> players(N);
+    for (int i = 0; i < N; i++) {
+        cin >> players[i];
+    }
+    vector<pll> left_players[N / 2 + 1];
+    vector<pll> right_players[N / 2 + 1];
+
+    for (int i = 0; i < (1 << N / 2); i++) {
+        int left_cnt = 0, right_cnt = 0;
+        ll left_weight = 0, right_weight = 0;
+        for (int j = 0; j < N / 2; j++) {
+            if (i & (1 << j)) {
+                left_weight += players[j];          // 작은게 나중에 옴
+                right_weight += players[N / 2 + j]; // 작은게 나중에 옴
+                left_cnt++, right_cnt++;
+            }
+        }
+        left_players[left_cnt].push_back({i, left_weight});
+        right_players[right_cnt].push_back({right_weight, i});
+    }
+    for (int i = 0; i < N / 2; i++) {
+        sort(right_players[i].begin(), right_players[i].end());
+    }
+
+    ll ans_i = 0, ans_j = 0, ans_min = LONG_LONG_MAX;
+    for (int i = 0; i < N / 2; i++) {
+        for (pll item : left_players[i]) {
+            int j = N / 2 - i;
+            vector<pll>::iterator ub = upper_bound(
+                right_players[j].begin(), right_players[j].end(), item.second);
+            vector<pll>::iterator lb = lower_bound(
+                right_players[j].begin(), right_players[j].end(), item.second);
+            if (ub == right_players[j].end()) {
+                if (lb == right_players[j].end())
+                    continue;
+                int idx = lb - right_players[j].begin();
+                if (ans_min >= abs(right_players[j][idx].first - item.second)) {
+                    ans_min = abs(right_players[j][idx].first - item.second);
+                    ans_i = item.first;
+                    ans_j = right_players[j][idx].second;
+                }
+            } else if (lb == right_players[j].end()) {
+                int idx = ub - right_players[j].begin();
+                if (ans_min >= abs(right_players[j][idx].first - item.second)) {
+                    ans_min = abs(right_players[j][idx].first - item.second);
+                    ans_i = item.first;
+                    ans_j = right_players[j][idx].second;
+                }
+            } else {
+                int idx = lb - right_players[j].begin();
+                if (ans_min >= abs(right_players[j][idx].first - item.second)) {
+                    ans_min = abs(right_players[j][idx].first - item.second);
+                    ans_i = item.first;
+                    ans_j = right_players[j][idx].second;
+                }
+                idx = ub - right_players[j].begin();
+                if (ans_min >= abs(right_players[j][idx].first - item.second)) {
+                    ans_min = abs(right_players[j][idx].first - item.second);
+                    ans_i = item.first;
+                    ans_j = right_players[j][idx].second;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (i < N / 2) {
+            if ((1 << i) & ans_i)
+                cout << "1 ";
+            else
+                cout << "2 ";
+        } else {
+            if ((1 << i - N / 2) & ans_j)
+                cout << "1 ";
+            else
+                cout << "2 ";
+        }
+    }
+}
---
+++ C++/etc/Two_pointer/4357.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+ll get_power(ll base, ll prime, ll expo) {
+    if (expo == 0)
+        return 1;
+    ll temp = get_power(base, prime, expo / 2);
+    if (expo % 2 == 0)
+        return (temp * temp) % prime;
+    return ((temp * temp) % prime) * base % prime;
+}
+ll get_inverse(ll base, ll prime) { return get_power(base, prime, prime - 2); }
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    ll p, n, b;
+    while (cin >> p >> b >> n) {
+        ll MOD = (ll)sqrt(p - 1);
+        ll inverse_b = get_inverse(b, p);
+        ll giant_b = get_power(b, p, MOD);
+        ll baby_step = MOD;
+        ll giant_step = p / MOD;
+
+        unordered_map<ll, ll> babies, giants;
+        ll curr = n;
+        babies[n] = 0;
+        for (int i = 1; i < baby_step; i++) {
+            curr = (curr * inverse_b) % p;
+            if (curr == n)
+                break;
+            babies[curr] = i;
+        }
+
+        curr = 1;
+        giants[0] = 1;
+        for (int i = 1; i < giant_step; i++) {
+            curr = (curr * giant_b) % p;
+            if (curr == 1)
+                break;
+            giants[i] = curr;
+        }
+
+        bool break_flag = false;
+        for (int giant = 0; giant < giant_step; giant++) {
+            ll curr = giants[giant];
+            if (babies.find(curr) != babies.end()) {
+                ll ans = giant * baby_step + babies[curr];
+                cout << ans << "\n";
+                break_flag = true;
+                break;
+            }
+        }
+
+        if (break_flag)
+            continue;
+        cout << "no solution\n";
+    }
+}
