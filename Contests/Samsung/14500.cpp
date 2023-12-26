023e271fad16df24fc9374f7582e64044c3c96fc
kisuk010203 <chriskisuk@gmail.com>
2023-12-26 23:18:27 +0900

    Samsung

---
+++ Contests/Samsung/14500.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 500
+#define ll long long
+int n, m;
+int board[MAX][MAX];
+bool visited[MAX][MAX];
+ll ans;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+    memset(visited, 0, sizeof(visited));
+    ans = 0;
+
+
+
+
+}
