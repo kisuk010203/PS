2ffa905ec94b8e0ec39d321f1a11a5f7d3b4dd84
kisuk010203 <chriskisuk@gmail.com>
2024-03-31 01:41:53 +0900

    Format files

--- rust_ps/src/data_structures/priority_queue/prob_1715.rs
+++ rust_ps/src/data_structures/priority_queue/prob_1715.rs
@@ -1,13 +1,16 @@
-use std::io::{stdin, stdout, BufReader, BufRead, BufWriter, Write};
-use std::collections::BinaryHeap;
 use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
 
 fn prob_1715() {
-    let (mut reader, mut writer) = (BufReader::new(stdin().lock()), BufWriter::new(stdout().lock()));
+    let (mut reader, mut writer) = (
+        BufReader::new(stdin().lock()),
+        BufWriter::new(stdout().lock()),
+    );
     let mut buf = String::new();
     let mut heap: BinaryHeap<Reverse<i64>> = BinaryHeap::new();
     let mut answer: i64 = 0;
-    
+
     reader.read_line(&mut buf).unwrap();
     let n = buf.trim().parse::<i64>().unwrap();
     for _ in 0..n {
