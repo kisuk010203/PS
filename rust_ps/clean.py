01017bd13933f88c89879e592c19b2d31310def3
kisuk010203 <chriskisuk@gmail.com>
2024-03-30 18:55:34 +0900

    Remove debugging prints

--- rust_ps/clean.py
+++ rust_ps/clean.py
@@ -4,10 +4,8 @@ with open("Cargo.toml", "r") as file:
     cargo_toml = toml.load(file)
 
 bins = cargo_toml.get('bin', [])
-print(bins)
 bins = [dict(t) for t in set([tuple(d.items()) for d in bins])]
 bins.sort(key = lambda x: x['name'])
-print(bins)
 
 cargo_toml['bin'] = bins
 
